5ed64164767badf020da4eb595f2aeb57843db44
mohsinraza2999 <mohsinraza2999@gmail.com>
2025-10-25 08:46:48 -0700

    update two service files

--- app/services/llm/extract_skills_llm.py
+++ app/services/llm/extract_skills_llm.py
@@ -3,11 +3,12 @@ import app.services.llm.llm_ranking as llm_ranking
 from app.core.logger import get_logger
 from langchain.chains import ConversationChain
 from app.services.llm.prompt_templates import get_parsing_prompt
+from typing import Optional
 logger = get_logger(__name__)
 
 
 
-def parse_cv(cv_text: str, api_key: str, model: str):
+def parse_cv(cv_text: str, api_key: Optional[str] = None, model: Optional[str] = None):
     #openai.api_key = api_key
     logger.info("Starting skill extraction using LLM")
 
